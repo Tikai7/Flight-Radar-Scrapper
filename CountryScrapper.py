2735b43f91527b2c65bddb3293e9e4ed2ffa1044
Melissa <95175568+MelissATB@users.noreply.github.com>
2024-02-01 17:29:50 +0100

    Update

--- CountryScrapper.py
+++ CountryScrapper.py
@@ -1,12 +1,10 @@
 import bs4
 import pandas as pd
-import urllib
 import numpy as np
-
 from urllib import request
 
 class CountryScrapper():
-    def __init__(self,url,headers) -> None:
+    def __init__(self,url= "https://www.flightradar24.com/data/airports",headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}) -> None:
         self.url = url
         self.headers = headers
         req = request.Request(url, headers=self.headers)
@@ -17,13 +15,16 @@ class CountryScrapper():
         return self.page.find("table", class_="tbl_datatable")
     
     def get_countries(self):
+        table = self.get_table()
         tags = table.find_all("a",{"data-country": True})
         countries = [c.text for c in tags if c.text != ""]
         return countries
     
     def get_nb_airports(self):
+        table = self.get_table()
         nb = table.find_all("span", class_="gray pull-right")
         airports_number = [''.join(char for char in span.text if char.isdigit()) for span in nb]
+        return airports_number
     
     def create_dataframe(self):
         countries = self.get_countries()
@@ -31,8 +32,5 @@ class CountryScrapper():
         return pd.DataFrame(list(zip(countries,airports)),columns=["Country","Airports"])
     
     def scrapper(self):
-        table = self.get_table()
-        airports = self.get_countries()
-        nb_airports = self.get_nb_airports()
         df = self.create_dataframe()
         return df
