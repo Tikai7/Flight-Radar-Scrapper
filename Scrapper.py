a636c4b5a3997273a6f955144b9630db4db662f8
Tikai7 <halimi.abdelkrim@outlook.fr>
2024-02-03 22:55:00 +0100

    added a scrapper for aircrafts (name,number,company)

--- Scrapper.py
+++ Scrapper.py
@@ -2,3 +2,4 @@ class Scrapper():
     def __init__(self) -> None:
         self.url = "https://www.flightradar24.com/data/airports"
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+        self.data = None
